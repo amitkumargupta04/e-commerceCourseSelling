c50926f4780ed904b9d48c4c9aa399b573f3cede
Amit Kumar Gupta <amit1704gupta@gmail.com>
2025-07-22 22:07:59 +0530

    url changed

--- frontend/utils/utils.js
+++ frontend/utils/utils.js
@@ -1,1 +1,3 @@
-export const BACKEND_URL = "http://localhost:8080/api/v1";
+//export const BACKEND_URL = "http://localhost:8080/api/v1";
+
+export const BACKEND_URL = "https://e-commercecourseselling.onrender.com";
