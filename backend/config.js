c901e22508bc08ef678bb98cc029ee697720c0eb
Amit Kumar Gupta <amit1704gupta@gmail.com>
2025-07-22 18:21:58 +0530

    Push block resolve

--- backend/config.js
+++ backend/config.js
@@ -3,7 +3,7 @@ dotenv.config();
 
 const JWT_USER_PASSWORD = process.env.JWT_USER_PASSWORD;
 const JWT_ADMIN_PASSWORD = process.env.JWT_ADMIN_PASSWORD;
-//const STRIPE_SECRET_KEY ="sk_test_51RmvsmHbCZokRKghz9A9l45JHX2xfr8H8dON7Qnd4qBaLkp4BUYYzNA3MXCxboRAO8SpN0JUk19rxpeGJZHfHoDm00cbQGEJZC"
+
 
 const STRIPE_SECRET_KEY = process.env.stripeSecretKey;
 
